5c3ea6e601113c629fb11593876c1aaf3def6cfe
i3Ammar <aaabughaith21@cit.just.edu.jo>
2025-06-12 09:31:48 +0300

    reverse paginattion

--- afkat_art/api/pagination.py
+++ afkat_art/api/pagination.py
@@ -3,7 +3,6 @@ from rest_framework.pagination import PageNumberPagination, LimitOffsetPaginatio
 
 class GameAndArtLayoutPagination(PageNumberPagination, LimitOffsetPagination):
     default_limit = 2
-    page_size = 3
     limit_query_param = 'limit'
     offset_query_param = 'offset'
     max_limit = 100
